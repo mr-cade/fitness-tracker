bfdb1277212cb9233669e16c5f2460ec627fbe5a
mr-cade <cadelpetersen@gmail.com>
2020-09-23 14:52:17 -0600

    models

---
+++ models/workout.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workouts = new Schema(
+  {
+    day: {
+      type: Date,
+      default: () => new Date()
+    },
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise type"
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise name"
+        },
+        duration: {
+          type: Number,
+          required: "Enter an exercise duration in minutes"
+        },
+        weight: {
+          type: Number
+        },
+        reps: {
+          type: Number
+        },
+        sets: {
+          type: Number
+        },
+        distance: {
+          type: Number
+        }
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true
+    }
+  }
+);
+
+// adds a virtual (total duration) to schema
+workouts.virtual("totalDuration").get(function () {
+  // "reduce" array of exercises down to just the sum of their durations
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+});
+
+const Workout = mongoose.model("Workout", workouts);
+
+module.exports = Workout;
