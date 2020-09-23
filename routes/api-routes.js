c8f58d05b26fbab29565b6c7da4b5d1668f427c8
mr-cade <cadelpetersen@gmail.com>
2020-09-23 14:53:23 -0600

    api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,38 +1,53 @@
+// requirements
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
+// route to add workout to db
 router.post("/api/workouts", (req, res) => {
   Workout.create({})
     .then(dbWorkout => {
       res.json(dbWorkout);
     })
-    .catch(err => {
-      res.json(err);
+    .catch(error => {
+      res.json(error);
     });
 });
 
+// route to update workout in db
 router.put("/api/workouts/:id", ({ body, params }, res) => {
   Workout.findByIdAndUpdate(
     params.id,
     { $push: { exercises: body } },
-    // "runValidators" will ensure new exercises meet our schema requirements
+    // validation
     { new: true, runValidators: true }
   )
     .then(dbWorkout => {
       res.json(dbWorkout);
     })
-    .catch(err => {
-      res.json(err);
+    .catch(error => {
+      res.json(error);
     });
 });
 
+// route to retrieve completed workouts
 router.get("/api/workouts", (req, res) => {
   Workout.find()
     .then(dbWorkouts => {
       res.json(dbWorkouts);
     })
-    .catch(err => {
-      res.json(err);
+    .catch(error => {
+      res.json(error);
+    });
+});
+
+// route to delete workouts in db
+router.delete("/api/workouts", ({ body }, res) => {
+  Workout.findByIdAndDelete(body.id)
+    .then(() => {
+      res.json(true);
+    })
+    .catch(error => {
+      res.json(error);
     });
 });
 
@@ -42,18 +57,8 @@ router.get("/api/workouts/range", (req, res) => {
       console.log(dbWorkouts)
       res.json(dbWorkouts);
     })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-router.delete("/api/workouts", ({ body }, res) => {
-  Workout.findByIdAndDelete(body.id)
-    .then(() => {
-      res.json(true);
-    })
-    .catch(err => {
-      res.json(err);
+    .catch(error => {
+      res.json(error);
     });
 });
 
